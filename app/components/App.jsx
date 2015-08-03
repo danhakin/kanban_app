314726a8a893c50cfa3e9498682831cd66d98c0d
danhakin <mdvillalobos@gmail.com>
2015-08-03 19:52:24 +0200

    Rendered data with autogenerated ids usin node-uuid dependency

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import Note from './Note';
+import uuid from 'node-uuid';
 
 export default class App extends React.Component {
 	render() {
-		return <Note />;
+		const notes = [
+			{
+				id: uuid.v4(),
+				task: 'Learn Webpack'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Learn React'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Do Laundry'
+			},
+
+		];
+
+		return (
+			<div>
+				<ul>{notes.map(this.renderNote)}</ul>
+			</div>
+		);
+	}
+
+	renderNote(note) {
+		return (
+			<li key={`note${note.id}`}>
+				<Note value={note.task} />
+			</li>
+		);
 	}
 }
