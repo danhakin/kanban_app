6bce865bb02ecc65689f4447ebf02243c5117cb3
danhakin <mdvillalobos@gmail.com>
2015-08-03 22:49:38 +0200

    Connected new item button to data model

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,22 +3,33 @@ import Notes from './Notes';
 import uuid from 'node-uuid';
 
 export default class App extends React.Component {
-	render() {
-		const notes = [
-			{
-				id: uuid.v4(),
-				task: 'Learn Webpack'
-			},
-			{
-				id: uuid.v4(),
-				task: 'Learn React'
-			},
-			{
-				id: uuid.v4(),
-				task: 'Do Laundry'
-			},
+	constructor(props) {
+		super(props);
+	
+		this.state = 
+		{
+			notes : [
+				{
+					id: uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do Laundry'
+				},
 
-		];
+			]
+		};
+
+		this.addItem = this.addItem.bind(this);
+	}	
+
+	render() {
+		const notes = this.state.notes;
 
 		return (
 			<div>
@@ -27,7 +38,14 @@ export default class App extends React.Component {
 			</div>
 		);
 	}
+
 	addItem() {
 		console.log('add item');
+		this.setState({
+			notes : this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'New task' 
+			}])
+		});
 	}
 }
